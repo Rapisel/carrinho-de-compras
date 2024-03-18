64b620ff0682fec17a7b919d614e85594ec1f4c0
Rapisel <rafaelpimentel063@gmail.com>
2024-03-18 09:57:49 -0300

    first commit

---
+++ js/app.js
@@ -0,0 +1,39 @@
+    let totalGeral ;
+    limpar();
+
+    function adicionar() {
+//recuperar valores, nome do produto, quantidade e valor 
+    let produto = document.getElementById('produto').value;
+    let quantidade = document.getElementById('quantidade').value;
+// Verificar se o produto selecionado é válido
+    if (!produto || produto.trim() === "") {
+        alert("Selecione um produto válido.");
+        return;
+    }
+// Verificar se a quantidade inserida é válida
+    if (isNaN(quantidade) || quantidade <= 0) {
+        alert("Insira uma quantidade válida.");
+        return;
+    }
+    let nomeProduto = produto.split('-')[0];
+    let valorUnitário = parseFloat(produto.split('R$')[1]);
+//calcular o preço, o nosso subtotal
+    let preco = quantidade * valorUnitário; 
+//adicionar ao carrinho de compras 
+    let carrinho = document.getElementById('lista-produtos');
+    carrinho.innerHTML = carrinho.innerHTML + `<section class="carrinho__produtos__produto">
+    <span class="texto-azul">${quantidade}x</span> ${nomeProduto} <span class="texto-azul">R$${preco}</span>
+  </section>`;
+//atualizar o valor total
+    totalGeral = totalGeral + preco;
+    let Total = document.getElementById ('valor-total');
+    Total.textContent = `R$ ${totalGeral}`;
+    document.getElementById('quantidade').value = 0;
+}
+
+function limpar() {
+    totalGeral = 0;
+    document.getElementById('lista-produtos').innerHTML = '';
+    document.getElementById ('valor-total').textContent = 'R$ 0';
+
+}
